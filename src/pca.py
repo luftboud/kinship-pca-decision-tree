54f3b78c04f7fae2c91689aada9f45a61eb3deff
Vlad Danylyshyn <vladuslavsd@gmail.com>
2026-04-04 18:34:59 +0300

    PCA bug fix

--- src/pca.py
+++ src/pca.py
@@ -6,7 +6,7 @@ def compute_pca_embeddings(face: np.ndarray, max_features_amount: int = 10) -> n
     eigenvalues, eigenvectors = np.linalg.eigh(a_transp_a)
 
     idx = np.argsort(eigenvalues)[::-1]
-    eigenvectors = eigenvectors[idx]
+    eigenvectors = eigenvectors[:, idx]
 
     wk = eigenvectors[:, :max_features_amount]
     embeddings = wk.T @ face.T
