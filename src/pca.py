51c28f89449a6e1cb59c330f924a8c243fe50341
Vlad Danylyshyn <vladuslavsd@gmail.com>
2026-04-04 17:54:28 +0300

    Reverse sort bug fix

--- src/pca.py
+++ src/pca.py
@@ -16,7 +16,7 @@ def compute_pca_embeddings(face: np.ndarray, max_features_amount: int = 10) -> n
     a_transp_a = face.T @ face
     eigenvalues, eigenvectors = np.linalg.eigh(a_transp_a)
 
-    idx = np.argsort(eigenvalues)
+    idx = np.argsort(eigenvalues)[::-1]
     eigenvectors = eigenvectors[idx]
 
     wk = eigenvectors[:, :max_features_amount]
